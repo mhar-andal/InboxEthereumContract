577aa09fc3fae15342043d94ae679c529c17f20d
Mhar Andal <mharandal96@gmail.com>
2018-04-05 17:01:18 -0500

    Add deploy script to deploy to an ethereum provider

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'add phrase seed here',
+  'insert wallet provider here through infura'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const contractInstance = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  console.log('Contract deployed to', contractInstance.options.address);
+};
+deploy();
